d4079aa0ef72e7e6ad9fd4eec4d62d4bf5c55e02
John Schellinger <schellingerj1@gmail.com>
2024-08-14 20:19:20 -0400

    Ensure no empty names end up in the admin list

--- src/commands/addop/addop.ts
+++ src/commands/addop/addop.ts
@@ -4,8 +4,9 @@ export async function addop(serverName: string, playerName: string) {
   try {
     const adminPlayerNames = ((await getStackParameter(serverName, 'AdminPlayerNames')) || '').split(',');
     adminPlayerNames.push(playerName);
-    updateStackParameter(serverName, 'AdminPlayerNames', adminPlayerNames.join(','));
-    console.log('new admin player list:', adminPlayerNames);
+    const temp = adminPlayerNames.filter((name) => !!name).join(',');
+    updateStackParameter(serverName, 'AdminPlayerNames', temp);
+    console.log('new admin player list:', temp);
   } catch (err: any) {
     console.error(`failed to promote ${playerName}. ${err.message.toLowerCase()}`);
   }
--- src/commands/rmop/rmop.command.ts
+++ src/commands/rmop/rmop.command.ts
@@ -4,10 +4,11 @@ export async function rmop(serverName: string, playerName: string) {
   try {
     const adminPlayerNames = (await getStackParameter(serverName, 'AdminPlayerNames')).split(',') || [];
     const i = adminPlayerNames.indexOf(playerName);
-    const temp = [...adminPlayerNames.slice(0, i), ...adminPlayerNames.slice(i + 1, adminPlayerNames.length)];
-    updateStackParameter(serverName, 'AdminPlayerNames', temp.join(','));
-    console.log('new admin player list');
-    console.log(temp);
+    const temp = [...adminPlayerNames.slice(0, i), ...adminPlayerNames.slice(i + 1, adminPlayerNames.length)]
+      .filter((name) => !!name)
+      .join(',');
+    updateStackParameter(serverName, 'AdminPlayerNames', temp);
+    console.log('new admin player list:', temp);
   } catch (err: any) {
     console.error(`failed to demote ${playerName}. ${err.message.toLowerCase()}`);
   }
