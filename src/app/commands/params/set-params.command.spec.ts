d54552aab9b2a87a4726003c8f01676e4ce98abc
John Schellinger <schellingerj1@gmail.com>
2024-08-18 10:52:29 -0400

    Fix tests

--- src/app/commands/params/set-params.command.spec.ts
+++ src/app/commands/params/set-params.command.spec.ts
@@ -16,194 +16,159 @@ describe('setParams', () => {
   it('should accept valid admins', () => {
     const mockOptions = { admins: 'johndoe1,jane_doe' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([{ ParameterKey: ParamsKey.ADMINS, ParameterValue: mockOptions.admins }]);
-    expect(logTable).toHaveBeenCalledWith({ [ParamsKey.ADMINS]: mockOptions.admins });
-    expect(log).not.toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      params: [{ ParameterKey: ParamsKey.ADMINS, ParameterValue: mockOptions.admins }],
+      discard: [],
+    });
   });
 
   it('should discard admins when there is a space in the delimiter', () => {
     const mockOptions = { admins: 'johndoe1, jane_doe' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({ '-a, --admins': 'invalid name format for one or more admins' });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.ADMINS] });
   });
 
   it('should discard admins when there is a space in one name', () => {
     const mockOptions = { admins: 'john doe1,jane_doe' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({ '-a, --admins': 'invalid name format for one or more admins' });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.ADMINS] });
   });
 
   it('should discard admins when there is a symbol other than underscore in one name', () => {
     const mockOptions = { admins: 'john-doe1,jane_doe' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({ '-a, --admins': 'invalid name format for one or more admins' });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.ADMINS] });
   });
 
   it('should accept valid difficulty', () => {
     const mockOptions = { difficulty: 'peaceful' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([{ ParameterKey: ParamsKey.DIFFICULTY, ParameterValue: mockOptions.difficulty }]);
-    expect(logTable).toHaveBeenCalledWith({ [ParamsKey.DIFFICULTY]: mockOptions.difficulty });
-    expect(log).not.toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      params: [{ ParameterKey: ParamsKey.DIFFICULTY, ParameterValue: mockOptions.difficulty }],
+      discard: [],
+    });
   });
 
   it('should discard invalid difficulty', () => {
     const mockOptions = { difficulty: 'foo' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({ '-d, --difficulty': 'valid values are [peaceful|easy|normal|hard]' });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.DIFFICULTY] });
   });
 
   it('should accept valid gamemode', () => {
     const mockOptions = { gamemode: 'creative' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([{ ParameterKey: ParamsKey.GAMEMODE, ParameterValue: mockOptions.gamemode }]);
-    expect(logTable).toHaveBeenCalledWith({ [ParamsKey.GAMEMODE]: mockOptions.gamemode });
-    expect(log).not.toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      params: [{ ParameterKey: ParamsKey.GAMEMODE, ParameterValue: mockOptions.gamemode }],
+      discard: [],
+    });
   });
 
   it('should discard invalid gamemode', () => {
     const mockOptions = { gamemode: 'foo' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({
-      '-g, --gamemode': 'valid values are [creative|survival|adventure|spectator]',
-    });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.GAMEMODE] });
   });
 
   it('should accept valid mem', () => {
     const mockOptions = { mem: '4G' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([{ ParameterKey: ParamsKey.MEMORY, ParameterValue: mockOptions.mem }]);
-    expect(logTable).toHaveBeenCalledWith({ [ParamsKey.MEMORY]: mockOptions.mem });
-    expect(log).not.toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      params: [{ ParameterKey: ParamsKey.MEMORY, ParameterValue: mockOptions.mem }],
+      discard: [],
+    });
   });
 
   it('should discard invalid mem', () => {
     const mockOptions = { mem: 'foo' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({
-      '-m, --mem': 'valid values are [1G|2G|4G|8G|16G]',
-    });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.MEMORY] });
   });
 
   it('should accept valid playermax', () => {
     const mockOptions = { playermax: '20' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([{ ParameterKey: ParamsKey.PLAYERS_MAX, ParameterValue: mockOptions.playermax }]);
-    expect(logTable).toHaveBeenCalledWith({ [ParamsKey.PLAYERS_MAX]: mockOptions.playermax });
-    expect(log).not.toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      params: [{ ParameterKey: ParamsKey.PLAYERS_MAX, ParameterValue: mockOptions.playermax }],
+      discard: [],
+    });
   });
 
   it('should discard invalid playermax', () => {
     const mockOptions = { playermax: 'foo' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({
-      '-p, --playermax': 'must be 1 - 100',
-    });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.PLAYERS_MAX] });
   });
 
   it('should accept valid state', () => {
     const mockOptions = { state: 'stopped' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([{ ParameterKey: ParamsKey.SERVER_STATE, ParameterValue: mockOptions.state }]);
-    expect(logTable).toHaveBeenCalledWith({ [ParamsKey.SERVER_STATE]: mockOptions.state });
-    expect(log).not.toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      params: [{ ParameterKey: ParamsKey.SERVER_STATE, ParameterValue: mockOptions.state }],
+      discard: [],
+    });
   });
 
   it('should discard invalid state', () => {
     const mockOptions = { state: 'foo' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({
-      '-s, --state': 'valid values are [running|stopped]',
-    });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.SERVER_STATE] });
   });
 
   it('should accept valid viewdist', () => {
     const mockOptions = { viewdist: '10' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([{ ParameterKey: ParamsKey.VIEW_DIST, ParameterValue: mockOptions.viewdist }]);
-    expect(logTable).toHaveBeenCalledWith({ [ParamsKey.VIEW_DIST]: mockOptions.viewdist });
-    expect(log).not.toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      params: [{ ParameterKey: ParamsKey.VIEW_DIST, ParameterValue: mockOptions.viewdist }],
+      discard: [],
+    });
   });
 
   it('should discard viewdist less than 1', () => {
     const mockOptions = { viewdist: '0' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({
-      '-v, --viewdist': 'must be 1 - 20',
-    });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.VIEW_DIST] });
   });
 
   it('should discard viewdist greater than 20', () => {
     const mockOptions = { viewdist: 21 };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({
-      '-v, --viewdist': 'must be 1 - 20',
-    });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.VIEW_DIST] });
   });
 
   it('should accept valid whitelist', () => {
     const mockOptions = { whitelist: 'johndoe1,jane_doe' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([{ ParameterKey: ParamsKey.WHITELIST, ParameterValue: mockOptions.whitelist }]);
-    expect(logTable).toHaveBeenCalledWith({ [ParamsKey.WHITELIST]: mockOptions.whitelist });
-    expect(log).not.toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      params: [{ ParameterKey: ParamsKey.WHITELIST, ParameterValue: mockOptions.whitelist }],
+      discard: [],
+    });
   });
 
   it('should discard whitelist when there is a space in the delimiter', () => {
     const mockOptions = { whitelist: 'johndoe1, jane_doe' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({
-      '-w, --whitelist': 'invalid name format for one or more whitelisted players',
-    });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.WHITELIST] });
   });
 
   it('should discard whitelist when there is a space in one name', () => {
     const mockOptions = { whitelist: 'john doe1,jane_doe' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({
-      '-w, --whitelist': 'invalid name format for one or more whitelisted players',
-    });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.WHITELIST] });
   });
 
   it('should discard whitelist when there is a symbol other than underscore in one name', () => {
     const mockOptions = { whitelist: 'john-doe1,jane_doe' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([]);
-    expect(log).toHaveBeenCalledWith('discarded invalid parameters');
-    expect(logTable).toHaveBeenCalledWith({
-      '-w, --whitelist': 'invalid name format for one or more whitelisted players',
-    });
+    expect(output).toStrictEqual({ params: [], discard: [ParamsKey.WHITELIST] });
   });
 
   it('should accept a valid timezone', () => {
     const mockOptions = { timezone: 'America/New York' };
     const output = convertTo(mockOptions);
-    expect(output).toStrictEqual([{ ParameterKey: ParamsKey.TIMEZONE, ParameterValue: mockOptions.timezone }]);
-    expect(logTable).toHaveBeenCalledWith({ [ParamsKey.TIMEZONE]: mockOptions.timezone });
-    expect(log).not.toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      params: [{ ParameterKey: ParamsKey.TIMEZONE, ParameterValue: mockOptions.timezone }],
+      discard: [],
+    });
   });
 });
